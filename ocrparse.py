fca7c780df20db3275ab8237ca559e734c26460e
David Lu <85049418+d8lu@users.noreply.github.com>
2023-08-16 23:34:20 -0400

    asd

--- ocrparse.py
+++ ocrparse.py
@@ -1,7 +1,7 @@
 import pytesseract
 from pdf2image import convert_from_path
 import os
-from PyPDF2 import PdfWriter, PdfReader
+from PyPDF2 import PdfWriter, PdfReader, PdfFileMerger
 import io
 import sys
 # pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
@@ -11,24 +11,24 @@ def convert_pdf_to_searchable_pdf(pdf_path, output_path):
     try:
     # Convert PDF pages to images
         images = convert_from_path(pdf_path)
-
+        merger = PdfFileMerger()
         # Create a list to store image bytes for each page
         image_bytes_list = []
         for image in images:
             # Perform OCR on the image and get the text (without writing to a file)
             text = pytesseract.image_to_pdf_or_hocr(image, extension='pdf')
-            image_bytes_list.append(text)
+            image_bytes_list.append(text)       
         # print("made it here")
         # Create a new searchable PDF by combining the OCR output from all pages
         with open(output_path, 'wb') as f:
-            pdf_writer = PdfWriter()
             for image_bytes in image_bytes_list:
                 pdf_reader = PdfReader(io.BytesIO(image_bytes))
                 page = pdf_reader.pages[0]
                 page.scale(0.5, 0.5) #page.scale(zoom_factor_x, zoom_factor_y)
-                pdf_writer.add_page(page)
+                merger.append(pdf_reader)
                 # pdf_writer.add_page(PdfReader(io.BytesIO(image_bytes)).pages[0])
-            pdf_writer.write(f)
+            # pdf_writer.write(f)
+            merger.write(f)
 
         print("Searchable PDF created successfully.")
     except Exception as e:
